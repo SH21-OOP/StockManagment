d3cca7b76d45c188d811868988de079da105d501
SH21 <anosovigor405@gmail.com>
2020-02-24 16:26:24 +0300

    feat(Containers): implemented Product

---
+++ StockManagment/Containers/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StockManagment.StockManagment.Containers
+{
+    public struct Product
+    {
+        private readonly string _Name;
+        private readonly DateTime _Preparation;
+        private readonly DateTime _Expiration;
+
+        public string Name
+        {
+            get { return _Name; }
+        }
+
+        public DateTime Preparation
+        {
+            get { return _Preparation; }
+        }
+
+        public DateTime Expiration
+        {
+            get { return _Expiration; }
+        }
+
+        public Product(string name, DateTime preparation, DateTime expiration)
+        {
+            _Name = name;
+            _Preparation = preparation;
+            _Expiration = expiration;
+        }
+    }
+}
