218856bf1e9e08d56f606fdbacf9f637d2812fb0
SH21 <anosovigor405@gmail.com>
2020-02-24 16:27:12 +0300

    feat(DataExchange): implemented Notifier

---
+++ DataExchange/Notifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockManagment.DataExchange
+{
+    public class Notifier
+    {
+        private List<Delegate> _Subsribers;
+
+        public Notifier()
+        {
+            _Subsribers = new List<Delegate>();
+        }
+
+        public void Notify()
+        {
+            foreach (Action action in _Subsribers)
+            {
+                action.Invoke();
+            }
+        }
+        public void Subscribe(Action callback)
+        {
+            _Subsribers.Add(callback);
+        }
+    }
+}
