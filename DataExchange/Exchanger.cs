975d004c781a2c5688b36cd406f420dbeaaa14df
SH21 <anosovigor405@gmail.com>
2020-02-24 16:27:45 +0300

    feat(DataExchange): implemented Exchanger

---
+++ DataExchange/Exchanger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockManagment.DataExchange
+{
+    public class Exchanger<T> : Notifier
+    {
+        private T _Value;
+        private List<Delegate> _Subsribers;
+
+        public Exchanger()
+        {
+            _Subsribers = new List<Delegate>();
+        }
+        public T Value
+        {
+            get { return _Value; }
+        }
+
+        public void Set(T value)
+        {
+            _Value = value;
+            Notify();
+        }
+    }
+}
